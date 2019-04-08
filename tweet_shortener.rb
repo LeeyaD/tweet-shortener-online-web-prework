864b5e142a320cd3187c9e031192560a191918ed
Leeya Davis <leeya463@gmail.com>
2019-04-08 01:44:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,5 +2,8 @@ dictionary = {"hello": 'hi', "to, two, too": '2', "for, four": '4', "be": 'b',
   "you": 'u', "at": "@", "and": "&" }
 
 def word_substituter(tweet)
+  tweet.split(" ").collect do |w|
+    w == dictionary.keys ? w.gsub(w, dictionary.value) : w
+  end
 
 end
