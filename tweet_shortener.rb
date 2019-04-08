a461e1b0aea7ce01562b4890aafda3cd3906eea7
Leeya Davis <leeya463@gmail.com>
2019-04-08 01:37:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+dictionary = {"hello": 'hi', "to, two, too": '2', "for, four": '4', "be": 'b',
+  "you": 'u', "at": "@", "and": "&" }
+
+"hello" becomes 'hi'
+"to, two, too" become '2'
