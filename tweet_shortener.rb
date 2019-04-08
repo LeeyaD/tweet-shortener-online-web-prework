a22b920789bad28ebba5b939923dd34c572d246e
Leeya Davis <leeya463@gmail.com>
2019-04-08 01:39:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,6 @@
 dictionary = {"hello": 'hi', "to, two, too": '2', "for, four": '4', "be": 'b',
   "you": 'u', "at": "@", "and": "&" }
 
-"hello" becomes 'hi'
-"to, two, too" become '2'
+def word_substituter(tweet)
+
+end
